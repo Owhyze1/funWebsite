bb1d47ab816433d541ee4aee012785425b1f0e0d
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-16 22:19:33 -0400

    moved into Home folder

---
+++ Home/index.js
@@ -0,0 +1,17 @@
+
+
+var button = document.getElementById("js");
+var interviewButton = document.getElementById("interviewing");
+var hackForLaButton = document.getElementById("hackforla");
+
+button.addEventListener("click", function() {
+  location.href = "Z:/home/owhyze1/coding/websitePractice/JavaScript/javascript.html" ;
+});
+
+interviewButton.addEventListener("click", function() {
+  location.href = "Z:/home/owhyze1/coding/websitePractice/html/interviewing.html";
+});
+
+hackforla.addEventListener("click", function() {
+  location.href = "Z:/home/owhyze1/coding/websitePractice/html/HackForLA.html";
+});
