c49d4f160c093a1910c365b2b8f74d7604d61b13
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-17 22:39:24 -0400

    File containing navigation links

---
+++ resources/links.js
@@ -0,0 +1,15 @@
+
+
+// HTML
+let hackForLA = "Z:/home/owhyze1/coding/websitePractice/HackForLA/HackForLA.html";
+let javascript = "Z:/home/owhyze1/coding/websitePractice/JavaScript/javascript.html"; let addRemoveItems = "Z:/home/owhyze1/coding/websitePractice/JavaScript/JS_Exercises/AddAndRemoveItems/addAndRemoveItems.html";
+let addToList = "Z:/home/owhyze1/coding/websitePractice/JavaScript/JS_Exercises/AddToList/addToList.html";
+let browserObjectModel = "Z:/home/owhyze1/coding/websitePractice/JavaScript/JS_Exercises/BrowserOM/browserOM.html";
+let createElements = "Z:/home/owhyze1/coding/websitePractice/JavaScript/JS_Exercises/CreateElements/createElements.html";
+let form = "Z:/home/owhyze1/coding/websitePractice/JavaScript/Form/form.html"
+let mouseEvents = "Z:/home/owhyze1/coding/websitePractice/JavaScript/JS_Exercises/MouseEvents/mouseEvents.html";
+
+// Exercises
+let smartwatch-store = "file:///Z:/home/owhyze1/coding/websitePractice/JavaScript/JS_Exercises/WatchShowcase/watch.html";
+
+// export { hackForLA, javascript };
