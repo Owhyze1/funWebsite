8718eafe581d1d8fae75dd68b5ff537b29bfcc09
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-18 15:45:13 -0400

    Added an example of Inheritance in JS classes

--- JavaScript/JS_Exercises/Classes/class.js
+++ JavaScript/JS_Exercises/Classes/class.js
@@ -13,5 +13,24 @@ class Person {
 
 var person = new Person("James", "Bond", 41);
 
-console.log(`His name is ${person.last}, ${person.first} ${person.last}`);
-console.log(person.howOld());
+// console.log(`His name is ${person.last}, ${person.first} ${person.last}`);
+// console.log(person.howOld());
+
+class Pilot extends Person {
+  constructor(experience, type, license) {
+    super('John', 'Wick', 35);
+    this.experience = experience;
+    this.type = type;
+    this.license = license;
+  }
+
+  getData = () => {
+    console.log(`${this.first} ${this.last}`);
+    console.log(`Experience ${this.experience} and Type: ${this.type}`);
+  }
+}
+
+const john = new Pilot(28, 'private', 'TC1234');
+john.getData();
+console.log(john);
+
