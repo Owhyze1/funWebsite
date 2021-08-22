010b463c91556352ad99d99db1ed579504f94854
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-22 11:56:14 -0400

    Added start and stop for timer interval

--- JavaScript/JS_Exercises/WatchShowcase/watchFeatures.js
+++ JavaScript/JS_Exercises/WatchShowcase/watchFeatures.js
@@ -1,4 +1,5 @@
 const countdown = require('./countdown');
+const timer = require('./timer');
 
 const timeButton = document.getElementById("time-button");
 const heartRateButton = document.getElementById("heart-rate-button");
@@ -6,9 +7,10 @@ const heartRateButton = document.getElementById("heart-rate-button");
 const divForTextElement = document.getElementById("watch-text-div");
 const textElement = document.getElementById("watch-text");
 
+let clock;
 
 if (timeButton) {
-  timeButton.addEventListener("click", function() {
+  timeButton.addEventListener('click', function() {
     var time = new Date();
     var hours = time.getHours();
     var minutes = time.getMinutes();
@@ -16,13 +18,17 @@ if (timeButton) {
 
     var time = `${hours}:${minutes}:${seconds}`;
     textElement.innerHTML = time;
+    console.log('Start time: ' + time);
 
-    var decreased = decrementTime(13, 00, 12);
-    console.log(decreased);
+    clock = setInterval(timer.start(time, textElement), 1000);
+  });
+}
+
+if (heartRateButton) {
+  heartRateButton.addEventListener('click', function() {
+    if (clock) clearInterval(clock);
 
 
-    var decreased = countdown.decrementTime(00, 15, 12);
-    console.log(decreased);
   });
 }
 
