d0d6cb303e225681454cd0284fac2b0c87edf123
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-21 22:28:37 -0400

    Implemented function to decrease sec, min, & hours

---
+++ JavaScript/JS_Exercises/WatchShowcase/countdown.js
@@ -0,0 +1,33 @@
+function decrementTime(hours, minutes, seconds) {
+  /*
+    Account for
+    - time less than 10
+    - negative time
+    - going from 00 to 59 for seconds AND minutes
+    - going from 00 to 23 for hours i.e. decrementing from midnight
+  */
+
+  // console.log(`Starting time = ${hours}:${minutes}:${seconds}`);
+
+  var lessThanTen = (time) => {
+    return (time >= 0 && time < 10) ? "0" + time : time;
+  }
+
+  var decreasingTimeFromZero = (time, timeMax) => {
+    return (time === -1) ? timeMax : time;
+  }
+
+  seconds = decreasingTimeFromZero(--seconds, 59);
+  if (seconds === 59)
+    minutes = decreasingTimeFromZero(--minutes, 59);
+  if (minutes === 59)
+    hours = decreasingTimeFromZero(--hours, 23);
+
+  hours = lessThanTen(hours);
+  minutes = lessThanTen(minutes);
+  seconds = lessThanTen(seconds);
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+exports.decrementTime = decrementTime;
