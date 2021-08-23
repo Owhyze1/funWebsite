dcdd7ef8c08bfba4387ef901079cdc47392c45b6
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-23 13:34:37 -0400

    Added clearInterval, increment/decrement timers

--- JavaScript/JS_Exercises/WatchShowcase/watchFeatures.js
+++ JavaScript/JS_Exercises/WatchShowcase/watchFeatures.js
@@ -1,41 +1,99 @@
-const countdown = require('./countdown');
-const timer = require('./timer');
-
 const timeButton = document.getElementById("time-button");
 const heartRateButton = document.getElementById("heart-rate-button");
 
 const divForTextElement = document.getElementById("watch-text-div");
 const textElement = document.getElementById("watch-text");
+const heartImage = document.getElementById("heart");
 
 let clock;
 
 if (timeButton) {
   timeButton.addEventListener('click', function() {
+    // move heart image and text div to correct position
+    clearInterval(clock);
+    heartImage.style.zIndex = 0;
+    heartImage.alt = 'watch screen';
+    divForTextElement.style.alignItems = 'center';
+    divForTextElement.paddingLeft = '0px';
+
+
     var time = new Date();
     var hours = time.getHours();
     var minutes = time.getMinutes();
     var seconds = time.getSeconds();
 
+    if (hours < 10) hours = '0' + hours;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+
     var time = `${hours}:${minutes}:${seconds}`;
     textElement.innerHTML = time;
-    console.log('Start time: ' + time);
 
-    clock = setInterval(timer.start(time, textElement), 1000);
+    let start = function(htmlElement) {
+      let output = htmlElement.innerHTML;
+      output = output.split(':');
+      let h = Number(output[0]);
+      let m = Number(output[1]);
+      let s = Number(output[2]);
+      htmlElement.innerHTML = incrementTime(h, m, s);
+    }
+    clock = setInterval(function() { start(textElement) },1000);
   });
 }
 
 if (heartRateButton) {
   heartRateButton.addEventListener('click', function() {
-    if (clock) clearInterval(clock);
+    clearInterval(clock);
 
-
+    heartImage.style.zIndex = 3;
+    heartImage.alt = 'Pulsing Heart';
+    // heartImage.style.transitionProperty = 'width';
+    // heartImage.style.transitionDuration = '3s';
+    divForTextElement.style.alignItems = 'flex-end';
+    divForTextElement.style.paddingLeft = '5px';
+    textElement.innerHTML = '78';
   });
 }
 
+function lessThanTen(time) {
+  return (time >= 0 && time < 10) ? "0" + time : time;
+}
 
+function resetAtMax(time, timeMax) {
+  return (time === timeMax) ? 0 : time;
+}
 
+function incrementTime(hours, minutes, seconds) {
 
-// textElement.innerHTML = "hello";
-// console.log(textElement.innerHTML);
+  seconds = resetAtMax(++seconds, 60);
+  if (seconds === 0)
+    minutes = resetAtMax(++minutes, 60);
+  if (minutes === 0)
+    hours = resetAtMax(++hours, 24);
 
-// divForTextElement.style.alignItems = "flex-end";
+  seconds = lessThanTen(seconds);
+  minutes = lessThanTen(minutes);
+  hours = lessThanTen(hours);
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function decrementTime(hours, minutes, seconds) {
+  if (hours < 0 || minutes < 0 || seconds < 0)
+    return;
+
+  let decreasingTimeFromZero = (time, timeMax) => {
+    return (time === -1) ? timeMax : time;
+  }
+  seconds = decreasingTimeFromZero(--seconds, 59);
+  if (seconds === 59)
+    minutes = decreasingTimeFromZero(--minutes, 59);
+  if (minutes === 59)
+    hours = decreasingTimeFromZero(--hours, 23);
+
+  hours = lessThanTen(hours);
+  minutes = lessThanTen(minutes);
+  seconds = lessThanTen(seconds);
+
+  return `${hours}:${minutes}:${seconds}`;
+}
