cdea27ce8b1943a572d72e6557562b719aae044e
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-30 16:26:43 -0400

    Implemented order of operations for calculations

--- JavaScript/JS_Exercises/Calculator/calculator.js
+++ JavaScript/JS_Exercises/Calculator/calculator.js
@@ -13,8 +13,7 @@ if (digits && calculatorScreen) {
   for (let i = 0; i < digits.length; i++) {
     digits[i].addEventListener('click', function(e) {
       screenText += e.target.innerHTML;
-      calculatorScreen.innerHTML = screenText;
-      console.log(`Text: ${screenText}`);
+      screenText;
     });
   }
 
@@ -24,16 +23,16 @@ if (digits && calculatorScreen) {
     if (key >= 48 && key <= 57) {
       screenText += e.key;
     } else if (key === 13 || key === 61) {
-      screenText = addOperator(` = `, screenText);
+      console.log('Equal Key');
       calculate(screenText);
     } else if (key === 42) {
-      screenText = addOperator(` * `, screenText);
+      screenText = addOperator(`*`, screenText);
     } else if (key === 43) {
-      screenText = addOperator(` + `, screenText);
+      screenText = addOperator(`+`, screenText);
     } else if (key === 45) {
-      screenText = addOperator(` - `, screenText);
+      screenText = addOperator('-', screenText);
     } else if (key === 47) {
-      screenText = addOperator(` / `, screenText);
+      screenText = addOperator('/', screenText);
     }
     calculatorScreen.innerHTML = screenText;
   });
@@ -51,6 +50,16 @@ if (operators) {
   }
 }
 
+function addOperator(operator, expression) {
+  let length = expression.length;
+
+  // check to see if an operator was already added to the expression. If so, replace with new operator chosen
+  if (isOperator(expression[length - 1])) {
+    return expression.slice(0, length - 1) + operator;
+  }
+  return expression + operator;
+}
+
 if (clearButton) {
   clearButton.addEventListener('click', function() {
     deleteCurrentExpression();
@@ -60,9 +69,57 @@ if (clearButton) {
 
 if (equalButton) {
   equalButton.addEventListener('click', function() {
-    console.log('Click event for equal button');
-    calculate(screenText);
+    if (isOperator(screenText[screenText.length - 1]) || screenText === '') {
+      alert('Choose a number');
+    } else {
+      calculate(screenText);
+    }
   });
+}
+
+
+function calculate(string) {
+  let numbers = string.split(/\D/);
+  let op = string.split(/\d+/);
+  op = op.slice(1, op.length - 1);
+
+  console.log(string);
+
+  let multiplyOrDivide = (string, op, numbers, operator) => {
+    let index = op.indexOf(operator);
+    if (index === -1) {
+      return false;
+    } else {
+      let value;
+      if (operator === '*') {
+        value = numbers[index] * numbers[index + 1];
+      } else if (operator === '/') {
+        value = numbers[index] / numbers[index + 1];
+      }
+      op.splice(index, 1);
+      numbers.splice(index, 2, value);
+      // console.log('NEW');
+      // console.log(numbers);
+      // console.log(op);
+    }
+  }
+  multiplyOrDivide(string, op, numbers, '*');
+  multiplyOrDivide(string, op, numbers, '/');
+
+  let addOrSubtract = (string, op, numbers) => {
+    let result = numbers[0];
+    for (let i = 0; i < op.length; i++) {
+      if (op[i] === '+') {
+        result += Number(numbers[i + 1]);
+      } else if (op[i] === '-') {
+        result -= Number(numbers[i + 1]);
+      }
+    }
+    console.log(`Result: ${result}`);
+    return result;
+  }
+  let result = addOrSubtract(string, op, numbers);
+  calculatorScreen.innerHTML = String(result);
 }
 
 function deleteCurrentExpression() {
@@ -70,17 +127,9 @@ function deleteCurrentExpression() {
   currentOperator = '';
 }
 
-function calculate(string) {
-  let expression = string.split(' ');
-  console.log(expression);
-}
-
-function addOperator(operator, expression) {
-  let length = expression.length;
-  operator = ` ${operator} `;
-
-  if (expression[length - 1] === ' ') {
-    return expression.slice(0, length - 3) + operator;
+function isOperator(op) {
+  if (op === '+' || op === '-' || op === '/' || op === '*') {
+    return true;
   }
-  return expression + operator;
+  return false;
 }
