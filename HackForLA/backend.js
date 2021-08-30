12517c1e481689cc25d593d4ff7418b7fe825f21
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-08-30 15:49:06 -0400

    Added event listeners for links

---
+++ HackForLA/backend.js
@@ -0,0 +1,8 @@
+const postgresElem = document.getElementById('postgres');
+
+const postgresHref = 'https://stackoverflow.com/questions/55038942/fatal-password-authentication-failed-for-user-postgres-postgresql-11-with-pg/55039419';
+
+
+postgresElem.addEventListener('click', function() {
+  location.href = postgresHref;
+});
