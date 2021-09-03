0d9dd7a91b6528a96c36b8dd436427954e34d63d
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-09-02 20:49:25 -0400

    Add event listeners to buttons

--- Home/index.js
+++ Home/index.js
@@ -1,8 +1,14 @@
 // import { javascript } from 'links.js';
 
-var button = document.getElementById("js");
-var interviewButton = document.getElementById("interviewing");
-var hackForLaButton = document.getElementById("hackforla");
+const button = document.getElementById("js");
+const interviewButton = document.getElementById("interviewing");
+const hackForLaButton = document.getElementById("hackforla");
+const troubleshootingElem = document.getElementById('troubleshooting');
+const reactElem = document.getElementById('react');
+
+const troubleshootingHref = '/troubleshooting.html';
+const reactHref = '/reactNotes.html';
+
 
 button.addEventListener("click", function() {
   console.log('Button clicked');
@@ -17,4 +23,12 @@ interviewButton.addEventListener("click", function() {
 hackforla.addEventListener("click", function() {
   console.log('Opening Hack for LA page');
   location.href = 'HackForLA.html';
+});
+
+troubleshootingElem.addEventListener('click', function() {
+  location.href = troubleshootingHref;
+});
+
+reactElem.addEventListener('click', function() {
+  location.href = reactHref;
 });
